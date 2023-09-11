6d62b39572df14bac39eba795a69d435fde61634
peaceapple <astonreba@gmail.com>
2023-09-11 13:54:16 +0300

    Fix import issue

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import Game from './Game';
 const StarMatch = () => {
 	const [gameId, setGameId] = useState(1);
 	return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)}/>;
--- src/components/PlayNumber.js
+++ src/components/PlayNumber.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const PlayNumber = props => (
     <button
       className="number"
